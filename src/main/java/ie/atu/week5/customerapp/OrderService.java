548fe663883e34ad9072d10a3f78ca589024a278
Mark Veerasingam <markveer70@gmail.com>
2024-10-19 21:28:19 +0100

    created OrderService for Order CRUD Logic

---
+++ src/main/java/ie/atu/week5/customerapp/OrderService.java
@@ -0,0 +1,40 @@
+package ie.atu.week5.customerapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public ResponseEntity<List<Order>> getOrdersByCustomerId(String customerId) {
+        List<Order> orders = orderRepository.findByCustomerId(customerId);
+        return ResponseEntity.ok(orders);
+    }
+
+    public Order createOrder(Order order){
+        Order savedOrder = orderRepository.save(order);
+        return savedOrder;
+    }
+
+    public ResponseEntity<Void> deleteOrder(String id){
+        if(orderRepository.existsById(id)) {
+            orderRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
